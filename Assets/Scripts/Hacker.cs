c3f415d7a5b0d33082583f6ef0e330de01ca30aa
F. Stephen Kirschbaum <fskirschbaum@gmail.com>
2019-04-29 21:46:32 -0500

    Adds enum for screen state and updates code to start using

--- Assets/Scripts/Hacker.cs
+++ Assets/Scripts/Hacker.cs
@@ -8,6 +8,9 @@ public class Hacker : MonoBehaviour
     // Game State
     int m_Level;
 
+    enum Screen { MainMenu, Password, Win };
+    Screen m_CurrentScreen = Screen.MainMenu;
+
     private void Start ()
     {
         ShowMainMenu();
@@ -15,37 +18,45 @@ public class Hacker : MonoBehaviour
 
     private void OnUserInput (string input)
     {
-        switch (input.ToUpper())
+        if (input.ToUpper() == "MENU")
         {
-            case "MENU":
-                ShowMainMenu();
-                break;
-            case "1":
-            case "2":
-            case "3":
-                m_Level = Int32.Parse(input);
-                StartGame();
-                break;
-            case "007":
-                Terminal.WriteLine("Welcome Mr. Bond, please choose a level");
-                break;
-            case "GOD":
-                Terminal.WriteLine("God Mode Activated");
-                break;
-            default:
-                Terminal.WriteLine("Invalid Input");
-                break;
+            ShowMainMenu();
+        }
+
+        if (m_CurrentScreen == Screen.MainMenu)
+        {
+            switch (input.ToUpper())
+            {
+                case "1":
+                case "2":
+                case "3":
+                    m_Level = Int32.Parse(input);
+                    StartGame();
+                    break;
+                case "007":
+                    Terminal.WriteLine("Welcome Mr. Bond, please choose a level");
+                    break;
+                case "GOD":
+                    Terminal.WriteLine("God Mode Activated");
+                    break;
+                default:
+                    Terminal.WriteLine("Invalid Input");
+                    break;
+            }
         }
     }
 
     private void StartGame()
     {
+        m_CurrentScreen = Screen.Password;
         Terminal.WriteLine("You have chosen level " + m_Level);
+        Terminal.WriteLine("Please enter your password: ");
     }
 
     private void ShowMainMenu ()
     {
         m_Level = 0;
+        m_CurrentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
         Terminal.WriteLine("Welcome to the WM2000 Learning Terminal");
         Terminal.WriteLine("");
